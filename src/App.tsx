a6eb25781d8833f926abb8a66db2bf5e2fe62819
Yichao <liuyichao200129@gmail.com>
2023-12-07 15:05:00 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type TPoint = {
+  x: number;
+  y: number;
+}
+
 function App() {
+  const [points, setPoints] = useState<TPoint[]>([]);
+  const [poped, setPoped] = useState<TPoint[]>([]);
+
+  const handleClick = (e: React.MouseEvent) => {
+    setPoints([...points, { x: e.clientX, y: e.clientY }]);
+  };
+
+  const handleUndo = () => {
+    console.log('Undo');
+    const newPoints = [...points];
+    const newPoped = [...poped];
+    const pop = newPoints.pop();
+    if (pop) {
+      newPoped.push(pop);
+      setPoped(newPoped);
+    }
+    setPoints(newPoints);
+  };
+
+  const handleRedo = () => {
+    console.log('Redo');
+
+    if (poped.length === 0) {
+      return;
+    }
+    
+    const newPoints = [...points];
+    newPoints.push(poped[0]);
+    setPoints(newPoints);
+
+    const newPoped = [...poped];
+    newPoped.shift();
+    setPoped(newPoped);
+  };
+
+  console.log(points);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleUndo}>Undo</button>
+      <button onClick={handleRedo}>Redo</button>
+      <div className="App" onClick={handleClick}>
+        {points.map((point, index) => <div className='Point' style={{
+          left: point.x - 25,
+          top: point.y - 25,
+          width: 50,
+          height: 50,
+        }} key={index}></div>)}
+      </div>
     </div>
+
   );
 }
 
